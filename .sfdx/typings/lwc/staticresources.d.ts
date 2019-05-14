6d81da406442d1dcabf12e7dd155e84e754036e6
Amit Singh <amitsingh4@deloitte.com>
2019-05-14 12:46:57 +0530

    Added Static Resource

--- .sfdx/typings/lwc/staticresources.d.ts
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -2,3 +2,7 @@ declare module "@salesforce/resourceUrl/YeurDreaminLogo" {
     var YeurDreaminLogo: string;
     export default YeurDreaminLogo;
 }
+declare module "@salesforce/resourceUrl/deloittelogo" {
+    var deloittelogo: string;
+    export default deloittelogo;
+}
